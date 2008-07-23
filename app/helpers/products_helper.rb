cb37fc147163a0ac6ba64bbbbb83241c229af6b6
zenhip <zenhip@gmail.com>
2008-07-23 21:57:50 +0300

    products_helper -> product_availability_info

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -3,5 +3,11 @@ module ProductsHelper
   def product_name(product)
      h(product.name)
   end
-  	
+  
+  def product_availability_info
+    "<b>Kopā (noliktavā):</b>
+		daudzums:<b> #{@product.batches_quantity_total} (#{@product.batches_quantity})</b>,
+		rezervēti:<b> #{@product.batches_reserved_total} (#{@product.incomplete_orders_quantity})</b>,
+		brīvi:<b> #{@product.batches_available_total} (#{@product.batches_available})</b><br />"
+  end
 end
