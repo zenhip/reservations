7828c185570529e951eb1a4a5da97239fc03b08c
zenhip <zenhip@gmail.com>
2008-07-25 16:46:35 +0300

    those views..

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -6,8 +6,8 @@ module ProductsHelper
   
   def product_availability_info
     "<b>Kopā (noliktavā):</b>
-		daudzums:<b> #{@product.batches_quantity_total} (#{@product.batches_quantity})</b>,
-		rezervēti:<b> #{@product.batches_reserved_total} (#{@product.incomplete_orders_quantity})</b>,
-		brīvi:<b> #{@product.batches_available_total} (#{@product.batches_available})</b><br />"
+		daudzums:<b> #{@product.batches_quantity}</b>,
+		rezervēti:<b> #{@product.incomplete_orders_quantity}</b>,
+		brīvi:<b> #{@product.batches_available}</b><br />"
   end
 end
